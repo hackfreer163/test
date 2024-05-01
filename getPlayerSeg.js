9c62a5a25f3a158c21f1263be0f5183880f69180
hackfreer163 <66307756+hackfreer163@users.noreply.github.com>
2024-05-01 15:38:23 +0800

    Update getPlayerSeg.js

--- getPlayerSeg.js
+++ getPlayerSeg.js
@@ -1,4 +1,4 @@
-handlers.GetPlayersInSegmentSample = function (args, context) {
+handlers.GetPlayersInSegmentSample1337 = function (args, context) {
 
     /*
         Sample code to use the GetPlayersInSegment API to process the player profiles in a Segment.
